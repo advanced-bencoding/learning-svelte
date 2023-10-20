4746d508e090478b43e9b8faa18c299851a727e3
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2023-10-20 11:59:03 +0530

    skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<div class="app-container" >
+    <nav class="header-nav flex-row">
+        <div class="flex-row">
+            <p>icon</p>
+            <p>KIT.SVELTE.DEV</p>
+        </div>
+        <div class="flex-row">
+            <p>searchbar</p>
+        </div>
+        <div class="flex-row">
+            <p>Docs</p>
+            <p>Tutorial</p>
+            <p>Svelte</p>
+            <div class="flex-row header-nav__icons">
+                icons
+            </div>
+        </div>
+    </nav>
+    <div>
+        <slot />
+    </div>
+</div>
+
+<script>
+    import '../styles/global.css'
+    import '../styles/layout.css';
+</script>
