8f27db486408442a3c55dc71b5d5fd4de2479239
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2023-10-29 23:27:32 +0530

    drawer with slide in

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
     import '../styles/global.css';
     import '../styles/layout.css';
+  import Drawer from './components/Drawer/Drawer.svelte';
     import Navbar from './components/Navbar/Navbar.svelte';
 </script>
 
 <div class="app-container" >
     <Navbar />
+    <Drawer />
     <div class="body-container">
         <slot />
     </div>
---
+++ src/routes/components/Drawer/Drawer.svelte
@@ -0,0 +1,14 @@
+<script>
+    import "./drawer.css";
+    import { drawerOpen } from "../../../stores/appStore";
+    import { slide } from "svelte/transition";
+</script>
+
+{#if $drawerOpen}
+    <div
+        class="drawer-container"
+        transition:slide={{ duration: 500, axis: "x" }}
+    >
+
+    </div>
+{/if}
