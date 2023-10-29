fec59a1d5f756591a98648343222a1a47dd691c3
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2023-10-29 22:47:53 +0530

    moved drawer var to store

--- src/routes/components/Navbar.svelte
+++ src/routes/components/Navbar.svelte
@@ -2,12 +2,35 @@
 
   import CustomizableTextBox from "../../common/components/CustomizableTextBox/CustomizableTextBox.svelte";
   import "./navbar.css";
+  import { drawerOpen } from "../../stores/appStore";
+
+  let wasDrawerVisible = false;
+
+  function openDrawer(){
+    if(!$drawerOpen){
+        wasDrawerVisible = false;
+        drawerOpen.set(true);
+        return;
+    }
+    wasDrawerVisible = true;
+    drawerOpen.set(false);
+  }
 
 </script>
 
 <nav class="nav flex-row">
     <div class="flex-row">
-        <img src="https://learn.svelte.dev/_app/immutable/assets/svelte-logo.5c5d7d20.svg" alt="logo.png" width="75rem" />
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+        <img
+            src="https://learn.svelte.dev/_app/immutable/assets/svelte-logo.5c5d7d20.svg"
+            alt="logo.png"
+            width="75rem"
+            class="nav__image"
+            class:nav__image--spin-clock={$drawerOpen}
+            class:nav__image--spin-anticlock={wasDrawerVisible && !$drawerOpen}
+            on:click={openDrawer}
+        />
         <span class="nav__title">KIT</span>
         <span class="nav__title nav__title--gray">.SVELTE.DEV</span>
     </div>
