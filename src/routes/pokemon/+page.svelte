fd07e8283e1d2497b2e8af3fda0d164324dbc8b3
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2023-11-27 10:56:02 +0530

    pokemon!

--- src/routes/pokemon/+page.svelte
+++ src/routes/pokemon/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import axios from 'axios';
+  import IconButton from '../../common/components/IconButton/IconButton.svelte';
 
     let apiCall = async () => {
         const result = await axios.get("https://pokeapi.co/api/v2/pokemon");
         return result.data.results;
     };
     console.log("ran");
-
     // game plan
     // choose random pokemon from balls displayed on homescreen
     // enter a fight with a random pokemon
@@ -18,8 +18,19 @@
     <p>loading...</p>
 {:then data}
     <ul>
-        {#each data as datum}
-            <li>{datum.name}</li>
-        {/each}
+        <div class="poke-grid">
+                {#each data as datum}
+                    <IconButton onClick={() => { console.log(datum.name) }}>
+                        <img slot="icon" src="/images/pokeball.png" alt="pokeball" width="150px" />
+                    </IconButton>
+                {/each}
+        </div>
     </ul>
 {/await}
+
+<style>
+    .poke-grid {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+    }
+</style>
